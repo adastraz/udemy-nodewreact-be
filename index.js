350f007897dcd7d8e8286fdaa80a45282cbdb734
adastraz <tylergosox@gmail.com>
2020-11-09 18:00:44 -0500

    login and logout
    create new entries in the users model, store them, able to compare googleID to distinguish returning users

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+const keys = require('./config/keys')
+require('./models/users')
 require('./services/passport')
+
+mongoose.connect(keys.mongoURI)
 const server = express()
+
+server.use(
+    cookieSession({ 
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+)
+server.use(passport.initialize())
+server.use(passport.session())
 
 require('./routes/auth-routes')(server)
 
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -4,4 +4,14 @@ module.exports = (server) => {
     server.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
 
     server.get('/auth/google/callback', passport.authenticate('google'))
+
+    server.get('/api/logout', (req, res) => {
+        req.logout()
+        res.send(req.user)
+    })
+
+    server.get('/api/current_user', (req, res) => {
+        res.send(req.user)
+    })
+
 }
